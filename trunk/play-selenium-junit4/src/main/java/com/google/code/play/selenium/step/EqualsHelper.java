afea08d6d8f719795fd988f515d72bc2cfcb5e0d
gslowikowski <gslowikowski@9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0>
2011-05-20 10:46:15 +0000

    License headers added, "Assert.assertTrue( boolean )" and "Assert.assertFalse( boolean )" methods changed to equivalent "SeleneseTestCase" class ones.

--- trunk/play-selenium-junit4/src/main/java/com/google/code/play/selenium/step/EqualsHelper.java
+++ trunk/play-selenium-junit4/src/main/java/com/google/code/play/selenium/step/EqualsHelper.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2006 ThoughtWorks, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+/**
+ * Modified com.thoughtworks.selenium.SeleneseTestBase.seleniumEquals( Object, Object ) method.
+ * - "System.out.println( ... )" statements removed
+ * - "seleniumNotEquals( String, String )" method added
+ */
 package com.google.code.play.selenium.step;
 
 import java.util.regex.Pattern;
