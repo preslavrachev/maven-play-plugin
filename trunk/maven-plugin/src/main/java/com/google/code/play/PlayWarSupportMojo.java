b875fb34e012881f4be93d91a2c3597fad93f8c3
gslowikowski <gslowikowski@9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0>
2011-10-09 11:30:06 +0000

    New mojo "war-support" for filtering "web.xml" file.

---
+++ trunk/maven-plugin/src/main/java/com/google/code/play/PlayWarSupportMojo.java
@@ -0,0 +1,171 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.google.code.play;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+//import java.util.Map;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Package Play! framework and Play! application as a WAR achive.
+ * 
+ * @author <a href="mailto:gslowikowski@gmail.com">Grzegorz Slowikowski</a>
+ * @goal war-support
+ * @phase prepare-package
+ */
+public class PlayWarSupportMojo
+    extends AbstractPlayMojo
+{
+    /**
+     * The directory for the generated (filtered) files.
+     * 
+     * @parameter expression="${project.build.directory}/play/tmp"
+     * @required
+     */
+    private File outputDirectory;
+
+    @Override
+    protected void/* String */resolvePlayId()
+    {
+        // String result = super.resolvePlayId();
+
+        if ( playId == null || "".equals( playId ) )
+        {
+            playId = "war";
+        }
+        // return result;
+    }
+
+    @Override
+    protected void internalExecute()
+        throws MojoExecutionException, MojoFailureException, IOException
+    {
+            File baseDir = project.getBasedir();
+            File confDir = new File( baseDir, "conf" );
+            File configurationFile = new File( confDir, "application.conf" );
+
+            ConfigurationParser configParser = new ConfigurationParser( configurationFile, playHome, playId );
+            configParser.parse();
+            //Map<String, File> modules = configParser.getModules();
+
+            /*File filteredApplicationConf =
+                *///filterApplicationConf( new File( baseDir, "conf/application.conf" ), modules );
+
+            /*File filteredWebXml = */filterWebXml( new File( playHome, "resources/war/web.xml" ), configParser.getApplicationName() );
+    }
+
+    private File filterWebXml( File webXml, String applicationName/*ConfigurationParser configParser*/ )
+        throws IOException
+    {
+        if ( !outputDirectory.exists() )
+        {
+            outputDirectory.mkdirs();
+        }
+        File result = new File( outputDirectory, "filtered-web.xml" );
+        BufferedReader reader = createBufferedFileReader( webXml, "UTF-8" );
+        try
+        {
+            BufferedWriter writer = createBufferedFileWriter( result, "UTF-8" );
+            try
+            {
+                getLog().debug( "web.xml file:" );
+                String line = reader.readLine();
+                while ( line != null )
+                {
+                    getLog().debug( "  " + line );
+                    if ( line.indexOf( "%APPLICATION_NAME%" ) >= 0 )
+                    {
+                        line = line.replace( "%APPLICATION_NAME%", applicationName/*configParser.getApplicationName()*/ );
+                    }
+                    if ( line.indexOf( "%PLAY_ID%" ) >= 0 )
+                    {
+                        line = line.replace( "%PLAY_ID%", playId );
+                    }
+                    writer.write( line );
+                    writer.newLine();
+                    line = reader.readLine();
+                }
+            }
+            finally
+            {
+                writer.close();
+            }
+        }
+        finally
+        {
+            reader.close();
+        }
+        return result;
+    }
+
+/* not needed
+    private File filterApplicationConf( File applicationConf, Map<String, File> modules )
+        throws IOException
+    {
+        if ( !outputDirectory.exists() )
+        {
+            outputDirectory.mkdirs();
+        }
+        File result = new File( outputDirectory, "filtered-application.conf" );
+        BufferedReader reader = createBufferedFileReader( applicationConf, "UTF-8" );
+        try
+        {
+            BufferedWriter writer = createBufferedFileWriter( result, "UTF-8" );
+            try
+            {
+                String line = reader.readLine();
+                while ( line != null )
+                {
+                    if ( !line.trim().startsWith( "#" ) && line.contains( "${play.path}" ) )
+                    {
+                        line = line.replace( "${play.path}", ".." );
+                    }
+                    writer.write( line );
+                    writer.newLine();
+                    line = reader.readLine();
+                }
+            }
+            finally
+            {
+                writer.close();
+            }
+        }
+        finally
+        {
+            reader.close();
+        }
+        return result;
+    }
+*/
+
+}
+
+// TODO
+/*
+ * 1. filtrowanie 'web.xml' - zrobione, ale mozna usprawnic: - wczytywac 'web.xml' odpowiednim readerem xml wylapujacym
+ * kodowanie w czasie rzeczywistym - co ze znakami nowej linii? - a moze wczytac inputstream'em? 3. ignorowanie plikow
+ * wymienionych w 'war.exclude' deleteFrom(war_path, app.readConf('war.exclude').split("|")) Skąd ja to wziąłem? Nie
+ * mogę znaleźć!
+ */
