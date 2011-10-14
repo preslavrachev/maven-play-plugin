5d49792229eb452946a4d74246cf8f391da1b19e
gslowikowski <gslowikowski@9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0>
2011-10-14 07:57:51 +0000

    "zip" mojo - a lot of changes: - added "zipIncludes" and "zipExcludes" parameters to select which project resources will be added to the archive, by default zipIncludes equals "app/**,conf/**,public/**,tags/**", and zipExcludes equals "", - added support for "module" and "module-resources" types of dependencies, - dependencies are not packaged by default, - added "zipDependencies" parameter with default value "false", set it to "true" if you want dependencies to be included in the archive.

--- trunk/maven-plugin/src/main/java/com/google/code/play/PlayZipMojo.java
+++ trunk/maven-plugin/src/main/java/com/google/code/play/PlayZipMojo.java
@@ -21,7 +21,9 @@ package com.google.code.play;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
@@ -30,7 +32,8 @@ import org.apache.maven.plugin.MojoFailureException;
 
 import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.ArchiverException;
-import org.codehaus.plexus.archiver.zip.ZipArchiver;
+import org.codehaus.plexus.archiver.manager.ArchiverManager;
+import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
 
 /**
  * Package Play! application as a ZIP achive.
@@ -47,27 +50,54 @@ public class PlayZipMojo
     /**
      * The directory for the generated ZIP.
      * 
-     * @parameter expression="${project.build.directory}"
+     * @parameter default-value="${project.build.directory}"
      * @required
+     * @since 1.0.0
      */
     private String outputDirectory;
 
     /**
-     * The name of the generated ZIP.
+     * Generated ZIP file name.
      * 
-     * @parameter expression="${project.build.finalName}"
+     * @parameter default-value="${project.build.finalName}"
      * @required
+     * @since 1.0.0
      */
     private String zipName;
 
-    // /**
-    // * Classifier to add to the generated ZIP. If given, the artifact will be an attachment instead.??? The classifier
-    // * will not be applied to the jar file of the project - only to the zip file.
-    // *
-    // * @parameter
-    // */
-    // private String classifier;
+    /**
+     * Include filter
+     * 
+     * @parameter expression="${play.zipIncludes}" default-value="app/**,conf/**,public/**,tags/**"
+     * @required
+     * @since 1.0.0
+     */
+    private String zipIncludes;
 
+    /**
+     * Exclude filter
+     * 
+     * @parameter expression="${play.zipExcludes}"
+     * @since 1.0.0
+     */
+    private String zipExcludes;
+
+    /**
+     * Package lib and modules directories or not
+     * 
+     * @parameter expression="${play.zipDependencies}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean zipDependencies;
+
+    /**
+     * To look up Archiver/UnArchiver implementations.
+     *
+     * @component role="org.codehaus.plexus.archiver.manager.ArchiverManager"
+     * @required
+     */
+    private ArchiverManager archiverManager;
+    
     protected void internalExecute()
         throws MojoExecutionException, MojoFailureException, IOException
     {
@@ -77,59 +107,99 @@ public class PlayZipMojo
             File destFile = new File( outputDirectory, zipName + ".zip" );
             // File destFile = new File( outputDirectory, getDestinationFileName() );
 
-            ZipArchiver zipArchiver = new ZipArchiver();
+            /*Zip*/Archiver zipArchiver = archiverManager.getArchiver( "zip" );//new ZipArchiver();
             zipArchiver.setDuplicateBehavior( Archiver.DUPLICATES_FAIL );// Just in case
             zipArchiver.setDestFile( destFile );
 
-            // application
-            zipArchiver.addDirectory( new File( baseDir, "app" ), "app/", null, null );
-            // conf
-            zipArchiver.addDirectory( new File( baseDir, "conf" ), "conf/", null, null );
-            // lib
-            Set<?> artifacts = project.getArtifacts();
-            for ( Iterator<?> iter = artifacts.iterator(); iter.hasNext(); )
+            getLog().debug( "Zip includes: " + zipIncludes );
+            getLog().debug( "Zip excludes: " + zipExcludes );
+            String[] includes = (zipIncludes != null ? zipIncludes.split(",") : null);
+            String[] excludes = (zipExcludes != null ? zipExcludes.split(",") : null);
+            zipArchiver.addDirectory( baseDir, includes, excludes);
+            
+            if (zipDependencies)
             {
-                Artifact artifact = (Artifact) iter.next();
-                if ( !artifact.isOptional() && 
-                                (Artifact.SCOPE_COMPILE.equals( artifact.getScope() ) || // project dependencies
-                                 Artifact.SCOPE_RUNTIME.equals( artifact.getScope() ) ) ) // project dependencies
-                {
-                    zipArchiver.addFile( artifact.getFile(), "lib/" + artifact.getFile().getName() );
-                }
+                Map<Artifact, String> moduleTypeArtifacts = processModuleDependencies(zipArchiver);
+                processJarDependencies(zipArchiver, moduleTypeArtifacts); 
             }
-            // public
-            zipArchiver.addDirectory( new File( baseDir, "public" ), "public/", null, null );
-            // tags
-            if ( new File( baseDir, "tags" ).isDirectory() )
-            {
-                zipArchiver.addDirectory( new File( baseDir, "tags" ), "tags/", null, null );
-            }
-
             zipArchiver.createArchive();
 
             project.getArtifact().setFile( destFile );
-
         }
         catch ( ArchiverException e )
         {
             throw new MojoExecutionException( "?", e );
         }
+        catch ( NoSuchArchiverException e )
+        {
+            throw new MojoExecutionException( "?", e );
+        }
     }
 
-    // private String getDestinationFileName()
-    // {
-    // StringBuffer buf = new StringBuffer();
-    // buf.append( zipName );
-    // if ( classifier != null && !"".equals( classifier ) )
-    // {
-    // if ( !classifier.startsWith( "-" ) )
-    // {
-    // buf.append( '-' );
-    // }
-    // buf.append( classifier );
-    // }
-    // buf.append( ".zip" );
-    // return buf.toString();
-    // }
+    private Map<Artifact, String> processModuleDependencies(Archiver archiver) throws ArchiverException, NoSuchArchiverException, IOException 
+    {
+        Map<Artifact, String> moduleTypeArtifacts = new HashMap<Artifact, String>();
+        
+        Set<?> artifacts = project.getArtifacts();
 
+        for ( Iterator<?> iter = artifacts.iterator(); iter.hasNext(); )
+        {
+            Artifact artifact = (Artifact) iter.next();
+            if ( "zip".equals( artifact.getType() ) )
+            {
+                if ( "module".equals( artifact.getClassifier() )
+                    || "module-resources".equals( artifact.getClassifier() ) )
+                {
+                    //System.out.println("module: " + artifact.getGroupId() + ":" + artifact.getArtifactId());
+                    
+                    // TODO-dorobic detekcje konfliktow nazw
+                    // System.out.println( "artifact: groupId=" + artifact.getGroupId() + ":artifactId="
+                    // + artifact.getArtifactId() + ":type=" + artifact.getType() + ":classifier="
+                    // + artifact.getClassifier() + ":scope=" + artifact.getScope() );
+                    File zipFile = artifact.getFile();
+                    String moduleName = artifact.getArtifactId();
+                    if ( moduleName.startsWith( "play-" ) )
+                    {
+                        moduleName = moduleName.substring( "play-".length() );
+                    }
+                    String moduleSubDir = String.format("%s-%s", moduleName, artifact.getVersion());
+                    archiver.addArchivedFileSet( zipFile, "modules/" + moduleSubDir + "/" );
+
+                    moduleTypeArtifacts.put( artifact, moduleSubDir );
+                    //System.out.println("module: " + artifact.getGroupId() + ":" + artifact.getArtifactId() + " added");
+                }
+            }
+        }
+        return moduleTypeArtifacts;
+    }
+
+    private void processJarDependencies(Archiver archiver, Map<Artifact, String> moduleTypeArtifacts) throws ArchiverException, NoSuchArchiverException, IOException 
+    {
+        Set<?> artifacts = project.getArtifacts();
+
+        for ( Iterator<?> iter = artifacts.iterator(); iter.hasNext(); )
+        {
+            Artifact artifact = (Artifact) iter.next();
+            if ( "jar".equals( artifact.getType() ) )
+            {
+                //System.out.println("jar: " + artifact.getGroupId() + ":" + artifact.getArtifactId());
+                File jarFile = artifact.getFile();
+                String libDir = "lib";
+                for (Artifact moduleArtifact: moduleTypeArtifacts.keySet())
+                {
+                    //System.out.println("checking module: " + moduleArtifact.getGroupId() + ":" + moduleArtifact.getArtifactId());
+                    if ( artifact.getGroupId().equals( moduleArtifact.getGroupId() )
+                        && artifact.getArtifactId().equals( moduleArtifact.getArtifactId() ) )
+                    {
+                        libDir = String.format( "modules/%s/lib", moduleTypeArtifacts.get( moduleArtifact ) );
+                        //System.out.println("checked ok - lib is " + libDir.getCanonicalPath());
+                        break;
+                    }
+                }
+                //System.out.println("jar: " + artifact.getGroupId() + ":" + artifact.getArtifactId() + " added to " + libDir);
+                archiver.addFile( jarFile, libDir + "/" + jarFile.getName() );
+            }
+        }
+    }
+    
 }
