8dc1dec69dd5100a852c94d6a8ce6afdc21d8e10
gslowikowski <gslowikowski@9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0>
2012-01-23 11:47:00 +0000

    [maven-release-plugin]  copy for tag test-projects-1.0.0-alpha6

---
+++ tags/test-projects-1.0.0-alpha6/findbugs/app/controllers/Application.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+//import models.*;
+
+public class Application extends Controller {
+
+    int foo;
+
+    public static void index() {
+        render();
+    }
+
+}
---
+++ tags/test-projects-1.0.0-alpha6/packagings/default/with-crud-module/play-1.2.x/readme.txt
@@ -0,0 +1,3 @@
+'crud' module defined as a dependency ('conf/dependencies.yml' for Play!, 'pom.xml' for Maven)
+
+run "mvn clean package -Pautodeps"
