7ecf5918ce58a4bc3998a5019f710ef233098a45
gslowikowski <gslowikowski@9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0>
2012-01-23 07:41:04 +0000

    [maven-release-plugin]  copy for tag plugin-1.0.0-alpha6

---
+++ tags/plugin-1.0.0-alpha6/play-maven-plugin/src/main/java/com/google/code/play/PlayDependenciesMojo.java
@@ -0,0 +1,300 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.google.code.play;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.shared.dependency.tree.DependencyTreeBuilderException;
+
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.archiver.manager.ArchiverManager;
+import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
+
+import org.codehaus.plexus.util.FileUtils;
+
+/**
+ * Extracts project dependencies to "lib" and "modules" directories.
+ * It's like Play! framework's "dependencies" command, but uses Maven dependencies,
+ * instead of "conf/dependencies.yml" file.
+ * 
+ * @author <a href="mailto:gslowikowski@gmail.com">Grzegorz Slowikowski</a>
+ * @goal dependencies
+ * @requiresDependencyResolution test
+ */
+public class PlayDependenciesMojo
+    extends AbstractDependencyProcessingPlayMojo
+{
+
+    /**
+     * Skip dependencies extraction.
+     * 
+     * @parameter expression="${play.dependenciesSkip}" default-value="false"
+     * @required
+     * @since 1.0.0
+     */
+    private boolean dependenciesSkip = false;
+
+    /**
+     * Should project's "lib" and "modules" subdirectories be cleaned before dependency resolution.
+     * If true, dependenciesOverwrite is meaningless.
+     * 
+     * @parameter expression="${play.dependenciesClean}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean dependenciesClean = false;
+
+    /**
+     * Should existing dependencies be overwritten.
+     * 
+     * @parameter expression="${play.dependenciesOverwrite}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean dependenciesOverwrite = false;
+
+    /**
+     * Should jar dependencies be processed. They are necessary for Play! Framework,
+     * but not needed for Maven build (Maven uses dependency mechanism).
+     * 
+     * @parameter expression="${play.dependenciesSkipJars}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean dependenciesSkipJars = false; // TODO-change to true
+
+    /**
+     * To look up Archiver/UnArchiver implementations.
+     * 
+     * @component role="org.codehaus.plexus.archiver.manager.ArchiverManager"
+     * @required
+     */
+    private ArchiverManager archiverManager;
+
+    protected void internalExecute()
+        throws MojoExecutionException, MojoFailureException, IOException
+    {
+        if ( dependenciesSkip )
+        {
+            getLog().info( "Dependencies extraction skipped" );
+            return;
+        }
+
+        try
+        {
+            if ( dependenciesClean )
+            {
+                File baseDir = project.getBasedir();
+                if ( !dependenciesSkipJars )
+                {
+                    FileUtils.deleteDirectory( new File( baseDir, "lib" ) );
+                }
+                FileUtils.deleteDirectory( new File( baseDir, "modules" ) );
+            }
+
+            Map<Artifact, File> moduleTypeArtifacts = processModuleDependencies();
+            if ( !dependenciesSkipJars )
+            {
+                Set<?> projectArtifacts = project.getArtifacts();
+                Set<Artifact> excludedArtifacts = new HashSet<Artifact>();
+                Artifact playSeleniumJunit4Artifact =
+                                getDependencyArtifact( projectArtifacts, "com.google.code.maven-play-plugin",
+                                                        "play-selenium-junit4", "jar" );
+                if ( playSeleniumJunit4Artifact != null )
+                {
+                    excludedArtifacts.addAll( getDependencyArtifacts( projectArtifacts, playSeleniumJunit4Artifact ) );
+                }
+                processJarDependencies( moduleTypeArtifacts, excludedArtifacts );
+            }
+        }
+        catch ( ArchiverException e )
+        {
+            // throw new MojoExecutionException( "Error unpacking file [" + file.getAbsolutePath() + "]" + "to ["
+            // + unpackDirectory.getAbsolutePath() + "]", e );
+            throw new MojoExecutionException( "?", e );
+        }
+        catch ( DependencyTreeBuilderException e )
+        {
+            throw new MojoExecutionException( "?", e );
+        }
+        catch ( NoSuchArchiverException e )
+        {
+            throw new MojoExecutionException( "?", e );
+        }
+    }
+
+    private Map<Artifact, File> processModuleDependencies()
+        throws ArchiverException, NoSuchArchiverException, IOException
+    {
+        Map<String, Artifact> moduleArtifacts = findAllModuleArtifacts( true );
+        Map<Artifact, File> moduleTypeArtifacts = decompressModuleDependencies( moduleArtifacts );
+        return moduleTypeArtifacts;
+    }
+
+    private Map<Artifact, File> decompressModuleDependencies( Map<String, Artifact> moduleArtifacts )
+        throws ArchiverException, NoSuchArchiverException, IOException
+    {
+        Map<Artifact, File> result = new HashMap<Artifact, File>();
+
+        File baseDir = project.getBasedir();
+        File modulesDirectory = new File( baseDir, "modules" );
+
+        for ( Map.Entry<String, Artifact> moduleArtifactEntry : moduleArtifacts.entrySet() )
+        {
+            String moduleName = moduleArtifactEntry.getKey();
+            Artifact moduleArtifact = moduleArtifactEntry.getValue();
+            checkPotentialReactorProblem( moduleArtifact );
+
+            if ( !Artifact.SCOPE_PROVIDED.equals( moduleArtifact.getScope() ) )
+            {
+                File zipFile = moduleArtifact.getFile();
+                String moduleSubDir = String.format( "%s-%s", moduleName, moduleArtifact.getVersion() );
+                File moduleDirectory = new File( modulesDirectory, moduleSubDir );
+                createModuleDirectory( moduleDirectory, dependenciesOverwrite
+                    || moduleDirectory.lastModified() < zipFile.lastModified() );
+                if ( moduleDirectory.list().length == 0 )
+                {
+                    UnArchiver zipUnArchiver = archiverManager.getUnArchiver( "zip" );
+                    zipUnArchiver.setSourceFile( zipFile );
+                    zipUnArchiver.setDestDirectory( moduleDirectory );
+                    zipUnArchiver.setOverwrite( false/* ??true */ );
+                    zipUnArchiver.extract();
+                    moduleDirectory.setLastModified( System.currentTimeMillis() );
+                }
+
+                result.put( moduleArtifact, moduleDirectory );
+            }
+        }
+        return result;
+    }
+
+    private void processJarDependencies( Map<Artifact, File> moduleTypeArtifacts, Collection<Artifact> excludedArtifacts )
+        throws ArchiverException, NoSuchArchiverException, IOException, DependencyTreeBuilderException
+    {
+        File baseDir = project.getBasedir();
+        Set<?> projectArtifacts = project.getArtifacts();
+        Set<Artifact> filteredArtifacts = new HashSet<Artifact>();
+        for ( Iterator<?> iter = projectArtifacts.iterator(); iter.hasNext(); )
+        {
+            Artifact artifact = (Artifact) iter.next();
+            if ( artifact.getArtifactHandler().isAddedToClasspath()
+                && !Artifact.SCOPE_PROVIDED.equals( artifact.getScope() ) && !excludedArtifacts.contains( artifact ) )
+            {
+                checkPotentialReactorProblem( artifact );
+                filteredArtifacts.add( artifact );
+            }
+        }
+
+        // modules/*/lib
+        for ( Map.Entry<Artifact, File> moduleTypeArtifactEntry : moduleTypeArtifacts.entrySet() )
+        {
+            Artifact moduleZipArtifact = moduleTypeArtifactEntry.getKey();
+            Set<Artifact> dependencySubtree = getModuleDependencyArtifacts( filteredArtifacts, moduleZipArtifact );
+            if ( !dependencySubtree.isEmpty() )
+            {
+                File modulePath = moduleTypeArtifactEntry.getValue();
+                File moduleLibDir = new File( modulePath, "lib" );
+                createLibDirectory( moduleLibDir );
+                for ( Artifact classPathArtifact : dependencySubtree )
+                {
+                    File jarFile = classPathArtifact.getFile();
+                    if ( dependenciesOverwrite )
+                    {
+                        FileUtils.copyFileToDirectory( jarFile, moduleLibDir );
+                    }
+                    else
+                    {
+                        if ( jarFile == null )
+                        {
+                            getLog().info( "null file" ); // TODO-???
+                        }
+                        // getLog().info(a.getGroupId()+":"+a.getArtifactId()+":"+a.getType()+":"+jarFile.getAbsolutePath());
+                        FileUtils.copyFileToDirectoryIfModified( jarFile, moduleLibDir );
+                    }
+                    filteredArtifacts.remove( classPathArtifact );
+                }
+            }
+        }
+        
+        // lib
+        if ( !filteredArtifacts.isEmpty() )
+        {
+            File libDir = new File( baseDir, "lib" );
+            createLibDirectory( libDir );
+            for ( Iterator<?> iter = filteredArtifacts.iterator(); iter.hasNext(); )
+            {
+                Artifact classPathArtifact = (Artifact) iter.next();
+                File jarFile = classPathArtifact.getFile();
+                if ( dependenciesOverwrite )
+                {
+                    FileUtils.copyFileToDirectory( jarFile, libDir );
+                }
+                else
+                {
+                    FileUtils.copyFileToDirectoryIfModified( jarFile, libDir );
+                }
+            }
+        }
+    }
+
+    private void createLibDirectory( File libDirectory )
+        throws IOException
+    {
+        if ( libDirectory.exists() )
+        {
+            if ( !libDirectory.isDirectory() )
+            {
+                throw new IOException( String.format( "\"%s\" is not a directory", libDirectory.getCanonicalPath() ) );
+            }
+        }
+        else
+        {
+            if ( !libDirectory.mkdirs() )
+            {
+                throw new IOException(
+                                       String.format( "Cannot create \"%s\" directory", libDirectory.getCanonicalPath() ) );
+            }
+        }
+    }
+
+    private void checkPotentialReactorProblem( Artifact artifact )
+        throws ArchiverException
+    {
+        File artifactFile = artifact.getFile();
+        if ( artifactFile.isDirectory() )
+        {
+            throw new ArchiverException(
+                                         String.format( "\"%s:%s:%s:%s\" dependent artifact's file is a directory, not a file. This is probably Maven reactor build problem.",
+                                                        artifact.getGroupId(), artifact.getArtifactId(),
+                                                        artifact.getType(), artifact.getVersion() ) );
+        }
+    }
+}
+
+// TODO
+// 1. Add name conflict detection for modules and jars
---
+++ tags/plugin-1.0.0-alpha6/play-maven-plugin/src/main/java/com/google/code/play/PlayStartServerMojo.java
@@ -0,0 +1,236 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.google.code.play;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Java;
+import org.apache.tools.ant.types.Path;
+
+/**
+ * Start Play! server. Based on <a
+ * href="http://mojo.codehaus.org/selenium-maven-plugin/start-server-mojo.html">selenium:start-server mojo</a>
+ * 
+ * @author <a href="mailto:gslowikowski@gmail.com">Grzegorz Slowikowski</a>
+ * @goal start-server
+ * @requiresDependencyResolution test
+ */
+public class PlayStartServerMojo
+    extends AbstractPlayServerMojo
+{
+    /**
+     * Play! id (profile) used for testing.
+     * 
+     * @parameter expression="${play.testId}" default-value="test"
+     * @since 1.0.0
+     */
+    protected String playTestId;
+
+//    /**
+//     * Enable logging mode.
+//     *
+//     * @parameter expression="${play.serverLogOutput}" default-value="true"
+//     * @since 1.0.0
+//     */
+//    private boolean serverLogOutput;
+
+    /**
+     * Skip goal execution
+     * 
+     * @parameter expression="${play.seleniumSkip}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean seleniumSkip;
+
+    /**
+     * Arbitrary JVM options to set on the command line.
+     * 
+     * @parameter expression="${play.serverProcessArgLine}"
+     * @since 1.0.0
+     */
+    private String serverProcessArgLine;
+
+    @Override
+    protected void internalExecute()
+        throws MojoExecutionException, MojoFailureException, IOException
+    {
+        if ( seleniumSkip )
+        {
+            getLog().info( "Skipping execution" );
+            return;
+        }
+
+        File playHome = getPlayHome();
+        File baseDir = project.getBasedir();
+
+        File confDir = new File( baseDir, "conf" );
+        File configurationFile = new File( confDir, "application.conf" );
+
+        ConfigurationParser configParser = new ConfigurationParser( configurationFile, playTestId );
+        configParser.parse();
+
+        boolean devMode = true;
+        String appModeStr = configParser.getProperty( "application.mode" );
+        if ( appModeStr != null )
+        {
+            appModeStr = appModeStr.toUpperCase();
+            devMode = "DEV".equals( appModeStr );
+        }
+        
+        int serverPort = 9000;
+        String serverPortStr = configParser.getProperty( "http.port" );
+        if ( serverPortStr != null )
+        {
+            serverPort = Integer.parseInt( serverPortStr );
+        }
+
+        File buildDirectory = new File( project.getBuild().getDirectory() );
+        File logDirectory = new File( buildDirectory, "play" );
+        if ( !logDirectory.exists() && !logDirectory.mkdirs() )
+        {
+            throw new MojoExecutionException( String.format( "Cannot create %s directory", logDirectory.getAbsolutePath() ) );
+        }
+
+        File pidFile = new File( logDirectory, "server.pid" );
+        if ( pidFile.exists() )
+        {
+            throw new MojoExecutionException( String.format( "Play! Server already started (\"%s\" file found)",
+                                                             pidFile.getName() ) );
+        }
+
+        Project antProject = createProject();
+        Path classPath = getProjectClassPath( antProject, playTestId );
+
+        Java java = new Java();
+        java.setProject( antProject );
+        java.setClassname( "com.google.code.play.PlayServerBooter" );
+        java.setFork( true );
+        java.setDir( baseDir );
+        java.setFailonerror( true );
+        java.setClasspath( classPath );
+        addSystemProperty( java, "play.home", playHome.getAbsolutePath() );
+        addSystemProperty( java, "play.id", playTestId );
+        addSystemProperty( java, "application.path", baseDir.getAbsolutePath() );
+        if ( !devMode )
+        {
+            addSystemProperty( java, "pidFile", pidFile.getAbsolutePath()/*"target/play/server.pid"*/ );
+        }
+
+        // if (serverLogOutput) {
+        File logFile = new File( logDirectory, "server.log" );
+        getLog().info( String.format( "Redirecting output to: %s", logFile.getAbsoluteFile() ) );
+        java.setOutput( logFile );
+        // }
+
+        if ( serverProcessArgLine != null )
+        {
+            String argLine = serverProcessArgLine.trim();
+            if ( !"".equals( argLine ) )
+            {
+                String[] args = argLine.split( " " );
+                for ( String arg : args )
+                {
+                    java.createJvmarg().setValue( arg );
+                    getLog().debug( "  Adding jvmarg '" + arg + "'" );
+                }
+            }
+        }
+
+        JavaRunnable runner = new JavaRunnable( java );
+        Thread t = new Thread( runner, "Play! Server runner" );
+        getLog().info( "Launching Play! Server" );
+        t.start();
+
+        // boolean timedOut = false;
+        
+        /*TimerTask timeoutTask = null;
+        if (timeout > 0) {
+            TimerTask task = new TimerTask() {
+                public void run() {
+                    timedOut = true;
+                }
+            };
+            timer.schedule( task, timeout * 1000 );
+            //timeoutTask = timer.runAfter(timeout * 1000, {
+            //    timedOut = true;
+            //})
+        }*/
+        
+        boolean started = false;
+        
+        getLog().info( "Waiting for Play! Server..." );
+
+        URL connectUrl = new URL( String.format( "http://localhost:%d", serverPort ) );
+        /*private*/ int verifyWaitDelay = 1000;
+        while ( !started )
+        {
+            //if (timedOut) {
+            //    throw new MojoExecutionException("Unable to verify if Play! Server was started in the given time ($timeout seconds)");
+            //}
+            
+            Exception runnerException = runner.getException();
+            if ( runnerException != null )
+            {
+                throw new MojoExecutionException( "Failed to start Play! Server", runnerException );
+            }
+
+            try
+            {
+                connectUrl.openConnection().getContent();
+                started = true;
+            }
+            catch ( Exception e )
+            {
+                // return false;
+            }
+
+            if ( !started )
+            {
+                try
+                {
+                    Thread.sleep( verifyWaitDelay );
+                }
+                catch ( InterruptedException e )
+                {
+                    throw new MojoExecutionException( "?", e );
+                }
+            }
+        }
+        
+        /*if (timeoutTask != null) {
+            timeoutTask.cancel();
+        }*/
+
+        getLog().info( "Play! Server started" );
+        
+        Exception startServerException = runner.getException();
+        if ( startServerException != null )
+        {
+            throw new MojoExecutionException( "?", startServerException );
+        }
+    }
+
+}
