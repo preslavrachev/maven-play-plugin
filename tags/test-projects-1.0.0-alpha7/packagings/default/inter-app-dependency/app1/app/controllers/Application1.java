fca65090cae8fe4fca85ed0d8bea118a8a0df0dc
gslowikowski <gslowikowski@9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0>
2012-03-20 22:07:24 +0000

    [maven-release-plugin]  copy for tag test-projects-1.0.0-alpha7

---
+++ tags/test-projects-1.0.0-alpha7/packagings/default/inter-app-dependency/app1/app/controllers/Application1.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+//import models.*;
+
+public class Application1 extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
